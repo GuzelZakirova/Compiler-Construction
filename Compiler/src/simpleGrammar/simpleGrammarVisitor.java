16371912d2b2f4a4749ec1cca7a5ee16de00a4b3
Ernest <58702017+ErnestMatskevich@users.noreply.github.com>
2024-02-21 19:14:37 +0300

    Add files via upload

---
+++ Compiler/src/simpleGrammar/simpleGrammarVisitor.java
@@ -0,0 +1,73 @@
+// Generated from C:/Users/guzel/IdeaProjects/CompilerMyGitHub/Compiler-Construction/Compiler/src/simpleGrammar/simpleGrammar.g4 by ANTLR 4.13.1
+package simpleGrammar;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link simpleGrammarParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface simpleGrammarVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link simpleGrammarParser#compilationUnit}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCompilationUnit(simpleGrammarParser.CompilationUnitContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link simpleGrammarParser#routine}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitRoutine(simpleGrammarParser.RoutineContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link simpleGrammarParser#block}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBlock(simpleGrammarParser.BlockContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link simpleGrammarParser#mainProg}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitMainProg(simpleGrammarParser.MainProgContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link simpleGrammarParser#statement}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitStatement(simpleGrammarParser.StatementContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link simpleGrammarParser#variableDeclaration}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitVariableDeclaration(simpleGrammarParser.VariableDeclarationContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link simpleGrammarParser#expressionStatement}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExpressionStatement(simpleGrammarParser.ExpressionStatementContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link simpleGrammarParser#expression}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExpression(simpleGrammarParser.ExpressionContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link simpleGrammarParser#operation}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOperation(simpleGrammarParser.OperationContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link simpleGrammarParser#type}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitType(simpleGrammarParser.TypeContext ctx);
+}
